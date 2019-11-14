34021b4cc7ccb5a5a26b32e90d5da17710b82434
jrbeverly <jrbeverly@live.com>
2019-11-13 21:37:18 -0500

    Pin versions of node

---
+++ tools/npm.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# Emit the current versions
+echo "Current versions"
+cat provision/nodelist
+
+mv provision/nodelist provision/nodelist.bak
+touch provision/nodelist
+while read line; do
+    echo "Working with ${line}"
+    input=(${line//=/ })
+    echo "Determining version for ${input}"
+    version=$(npm show ${input} version)
+    echo "Found version as ${version}"
+    echo "${input}=${version}" >> provision/nodelist
+done <provision/nodelist.bak
+
+#
+rm provision/nodelist.bak
+cat provision/nodelist
